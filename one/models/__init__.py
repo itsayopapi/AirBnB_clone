6413f0681b2299483fe1ec89db9ab9c407582acb
Ayocoding12 <gbemialihassan@gmail.com>
2023-02-12 04:59:28 -0800

    let's redo

--- one/models/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-'''Initializes the package'''
-# from models.base_model import BaseModel
-from models.engine import file_storage
-
-
-storage = file_storage.FileStorage()
-storage.reload()
